f647d2d4e2aaad3c79dabf9d1a4e03d5e1a59f85
Valentin Roche <54205303+titithen00b@users.noreply.github.com>
2022-10-26 13:51:43 +0200

    Create copie.bat

---
+++ copie.bat
@@ -0,0 +1,37 @@
+@echo off
+
+:start
+if not exist "Y:" ( goto error ) else ( goto copie_01 )
+goto copie_1 
+
+
+:copie_01
+::copie des fichiers pdf dans 01-TODO
+Y:
+cd "04-KO TECHNIQUE"
+for /R %%i in ( *.pdf ) do (
+    echo %%i
+    xcopy "%%i" "Y:\01-TODO" /S
+)
+echo Copie vers "Mathieu" terminee
+timeout 5 /nobreak >nul
+cls
+goto supr
+
+:supr
+echo Suppression desactive actuellement
+::Suppression des dossiers présent dans 04-KO TECHNIQUE
+forfiles /P "Y:\04-KO TECHNIQUE" /M * /C "cmd /c if @isdir==FALSE del @file"
+forfiles /P "Y:\04-KO TECHNIQUE" /M * /C "cmd /c if @isdir==TRUE rmdir /S /Q @file"
+timeout 5 /nobreak >nul
+cls
+goto eof
+
+:error
+echo Impossible de trouver le lecteur. Merci de le reconnecter sur Y:
+echo Le chemin est \\srv-ad\partage\Secrétaires\EUROCOMBLES Secrétaires\Dossiers Clients\RPA\01-SCAN
+goto eof
+
+:eof
+timeout 90m ping -t 127.0.0.1 > nul
+goto start
